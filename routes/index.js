94d00d0f3e9a1564df8bea2c39c85d09db1e5dfb
DanFerrer <danielferrer212@gmail.com>
2018-09-15 21:25:39 -0400

    change folder structure

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,76 @@
-const router = require('express').Router();
-const RedisServer = require('redis-server');
-const articles = require('../articles/articles.json');
-const multer  = require('multer');
-const path = require('path');
-const fs = require('fs');
+// const router = require('express').Router();
+// const RedisServer = require('redis-server');
+// const articles = require('../articles/articles.json');
+// const multer  = require('multer');
+// const path = require('path');
+// const fs = require('fs');
 
-var client, limiter, storage, upload;
+// var client, limiter, storage, upload;
 
-const server = new RedisServer(6379);
+// const server = new RedisServer(6379);
 
-function fibonacci (num) {
-  return num <= 1 ? num : fibonacci(num-1) + fibonacci(num-2);
-};
+// function fibonacci (num) {
+//   return num <= 1 ? num : fibonacci(num-1) + fibonacci(num-2);
+// };
 
-server.open().then(() => {
-  client = require('redis').createClient();
-  limiter = require('express-limiter')(router, client);
-  storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, '../articles/');
-    },
-    filename: function(req, file, cb) {
-      cb(null, `${file.fieldname}-${Date.now()}`)
-    }
-  });
-  upload = multer({storage: storage}).single('newArticle');
-});
+// server.open().then(() => {
+//   client = require('redis').createClient();
+//   limiter = require('express-limiter')(router, client);
+//   storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//       cb(null, '../articles/');
+//     },
+//     filename: function(req, file, cb) {
+//       cb(null, `${file.fieldname}-${Date.now()}`)
+//     }
+//   });
+//   upload = multer({storage: storage}).single('newArticle');
+// });
 
-router.use((req, res, next)  => {
-  console.log(`Running fibonacci for ${req.originalUrl}`);
-  console.log(fibonacci(34));
-  next();
-});
+// router.use((req, res, next)  => {
+//   console.log(`Running fibonacci for ${req.originalUrl}`);
+//   console.log(fibonacci(34));
+//   next();
+// });
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'connected to index route'});
-});
+// router.get('/', (req, res) => {
+//   res.status(200).json({ message: 'connected to index route'});
+// });
 
-router.get('/articles/:articleName', limiter({total: 20100, expire: 1000, lookup: 'connection.remoteAddress'}), (req, res) => {
-  let article = articles.find(ele => ele.slug === req.params.articleName);
+// router.get('/articles/:articleName', limiter({total: 20100, expire: 1000, lookup: 'connection.remoteAddress'}), (req, res) => {
+//   let article = articles.find(ele => ele.slug === req.params.articleName);
 
-  if (article) {
-    let htmlPath = path.join(__dirname, '..', `/articles/${article.slug}/${article.file}`);
+//   if (article) {
+//     let htmlPath = path.join(__dirname, '..', `/articles/${article.slug}/${article.file}`);
 
-    res.sendFile(htmlPath);
-  }
-});
+//     res.sendFile(htmlPath);
+//   }
+// });
 
-router.get('/articles/:articleName/download', (req, res, next) => {
-  let article = articles.find(ele => ele.slug === req.params.articleName);
+// router.get('/articles/:articleName/download', (req, res, next) => {
+//   let article = articles.find(ele => ele.slug === req.params.articleName);
 
-  if (article) {
-    let htmlPath = path.join(__dirname, '..', `/articles/${article.file}`);
+//   if (article) {
+//     let htmlPath = path.join(__dirname, '..', `/articles/${article.file}`);
 
-    res.download(htmlPath, article.file);
-  }
-});
+//     res.download(htmlPath, article.file);
+//   }
+// });
 
-router.post('/articles/:articleName/upload', limiter({total: 10, expire: 1000, lookup: 'connection.remoteAddress'}), upload.single('newArticle'), (req, res) => {
-  let article = articles.find(ele => ele.slug === req.params.articleName);
+// router.post('/articles/:articleName/upload', limiter({total: 10, expire: 1000, lookup: 'connection.remoteAddress'}), upload.single('newArticle'), (req, res) => {
+//   let article = articles.find(ele => ele.slug === req.params.articleName);
 
-  if (article) {
-    upload(req, res, function (err) {
-      if (err) return;
+//   if (article) {
+//     upload(req, res, function (err) {
+//       if (err) return;
 
-      res.send('File Uploaded!');
-    });
-  }
-});
-
-
+//       res.send('File Uploaded!');
+//     });
+//   }
+// });
 
 
 
-module.exports = router;
+
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,51 +1,51 @@
-const cluster = require('cluster');
-const os = require('os');
+// const cluster = require('cluster');
+// const os = require('os');
 
-if (cluster.isMaster) {
-  const numCPUS = process.env.numCPUS || os.cpus().length;
+// if (cluster.isMaster) {
+//   const numCPUS = process.env.numCPUS || os.cpus().length;
 
-  for (let i = 0; i < numCPUS; i++) {
-    cluster.fork();
-  }
+//   for (let i = 0; i < numCPUS; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('online', (worker) => {
-    console.log('Worker %d is online', worker.process.pid)
-  });
+//   cluster.on('online', (worker) => {
+//     console.log('Worker %d is online', worker.process.pid)
+//   });
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log('Worker %d died with code/signal %s. Restarting worker...', worker.process.pid, signal || code);
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log('Worker %d died with code/signal %s. Restarting worker...', worker.process.pid, signal || code);
 
-    cluster.fork();
-  });
-} else {
-  const express = require('express');
-  const routes = require('./routes');
-  const path = require('path');
-  const redis = require('redis');
-  const fs = require('fs');
+//     cluster.fork();
+//   });
+// } else {
+//   const express = require('express');
+//   const routes = require('./routes');
+//   const path = require('path');
+//   const redis = require('redis');
+//   const fs = require('fs');
 
-  const app = express();
-  const port = process.env.PORT || 8080;
+//   const app = express();
+//   const port = process.env.PORT || 8080;
 
-  app.use('/', routes);
+//   app.use('/', routes);
 
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: err
-    });
-  });
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: err
+//     });
+//   });
 
-  app.all('/*', (req, res) => {
-    res.send('process ' + process.pid + ' says hello!').end();
-  });
+//   app.all('/*', (req, res) => {
+//     res.send('process ' + process.pid + ' says hello!').end();
+//   });
 
-  app.listen(port, '0.0.0.0', (err) => {
-    if (err) {
-      console.log(err);
-    }
+//   app.listen(port, '0.0.0.0', (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
 
-    console.info(`🌎 Process ${process.pid} is listening to all incoming requests on port ${port} 🌎`);
-  });
-}
+//     console.info(`🌎 Process ${process.pid} is listening to all incoming requests on port ${port} 🌎`);
+//   });
+// }
